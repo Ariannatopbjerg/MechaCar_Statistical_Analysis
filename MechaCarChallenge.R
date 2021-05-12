daa609aa6ce885004c327f66a75e5045cdc3734c
Arianna Topbjerg <amt700@humboldt.edu>
2021-05-11 20:37:02 -0700

    Deliverable 2 and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-#Deliverable 1
+#DELIVERABLE 1
 
 #Load packages
 library(tidyverse)
@@ -13,3 +13,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Perform summary function
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar))
+
+#DELIVERABLE 2
+
+#Import and read file as a dataframe
+Suspension <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(Suspension)
+
+#Creat total summary dataframe
+total_summary <- Suspension %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Creat a lot summary dataframe
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
